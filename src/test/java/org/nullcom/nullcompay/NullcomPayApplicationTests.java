e7010edfcdd90975d608c7ee0d41fcb4b0517ccb
hjwnu <tlstjr01@gmail.com>
2024-06-07 13:04:46 +0900

    fix config

---
+++ src/test/java/org/nullcom/nullcompay/NullcomPayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nullcom.nullcompay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullcomPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
